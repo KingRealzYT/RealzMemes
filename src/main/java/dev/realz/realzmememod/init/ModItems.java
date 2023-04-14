b105f0b816358e76e37f383b5bd328abdf5d4050
Jonathan Enchackal <jonathansanie@gmail.com>
2023-04-14 15:57:29 -0500

    yea

--- src/main/java/dev/realz/realzmememod/init/ModItems.java
+++ src/main/java/dev/realz/realzmememod/init/ModItems.java
@@ -1,12 +1,16 @@
 package dev.realz.realzmememod.init;
 
 import dev.realz.realzmememod.RealzMemes;
+import dev.realz.realzmememod.items.IngotBase;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RealzMemes.MOD_ID);
+
+    public static final RegistryObject<Item> MEME_ESSENCE = ITEMS.register("meme_essence", IngotBase::new);
 
 }
---
+++ src/main/java/dev/realz/realzmememod/items/IngotBase.java
@@ -0,0 +1,29 @@
+package dev.realz.realzmememod.items;
+
+import dev.realz.realzmememod.init.ModItems;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import static dev.realz.realzmememod.RealzMemes.MOD_ID;
+
+public class IngotBase extends Item {
+    public IngotBase() {
+        super(new Item.Properties());
+    }
+
+    @SubscribeEvent
+    public void buildContents(CreativeModeTabEvent.Register event) {
+        event.registerCreativeModeTab(new ResourceLocation(MOD_ID, "realzmemes"), builder ->
+                builder.title(Component.translatable("item_group." + MOD_ID + ".realzmemes"))
+                        .icon(() -> new ItemStack(ModItems.MEME_ESSENCE
+                                .get()))
+                        .displayItems((params, output) -> {
+                            output.accept(ModItems.MEME_ESSENCE.get());
+                        })
+        );
+    }
+}
